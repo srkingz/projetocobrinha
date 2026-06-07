32f8eaa7e887ed97096b76d1e6d4b9c609a2968b
Gabriel Henrique de Souza Rodrigues <henriqueghzx@gmail.com>
2026-06-07 16:57:04 -0300

    Atualização Certa

--- src/config.py
+++ src/config.py
@@ -1,14 +1,13 @@
 # Configurações centrais do jogo (tela, cores e caminhos de arquivos).
 LARGURA_TELA = 800
 ALTURA_TELA = 600
-FPS = 8
+FPS = 60
 
-TITULO_JOGO = "Jogo da Cobrinha"
+TITULO_JOGO = "Projeto Final - Pygame"
 
 BRANCO = (255, 255, 255)
 PRETO = (0, 0, 0)
 CINZA = (212,212,212)
-VERDE = (0, 255, 0)
 
 CAMINHO_RECORDE = "data/recorde.txt"
 CAMINHO_SPRITES = "assets/imagens/spritesheet.bmp"
